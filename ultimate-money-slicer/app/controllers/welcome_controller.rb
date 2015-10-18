a9475c777c6aa38adf60b3fa8c1c1d44215d3c0d
lunete <claud@openmailbox.org>
2015-10-18 10:18:52 -0400

    got oauth half working

--- ultimate-money-slicer/app/controllers/welcome_controller.rb
+++ ultimate-money-slicer/app/controllers/welcome_controller.rb
@@ -1,21 +1,43 @@
 class WelcomeController < ApplicationController
   def homepage
       require 'slice'
+      #require 'uri'
 
-      oauth = Slice::OAuth.new("47d4d088", "0be08d72fd5cdb21f08eea76d2479832", "localhost:3000")
+
+
+      oauth = Slice::OAuth.new("47d4d088", "0be08d72fd5cdb21f08eea76d2479832", "http://localhost:3000")
       @auth_url = oauth.generate_auth_url
 
-    #   access_token = oauth.authenticate!
-      #
-    #   client = Slice::Client.new(access_token: access_token.token)
-    #   client.list_items
-    #   client.list_orders
-    #   client.create_order(orderTitle: 'Your Amazon.com order')
-    #   client.get_order(123)
-    #   client.update_order(123, orderTotal: 395)
-    #   client.list_items(limit: 3, offset: 10)
-    #   client.list_items(limit: 3, offset: 10).status
-    #   client.list_items(limit: 3, offset: 10).headers
-    #   client.list_items(limit: 3, offset: 10).body
+
+
+      #uri = URI.parse(@auth_url)
+      #auth_code = uri.host.split('=').first
+      #if oauth.code != nil
+
+
+
+        # params.require[:code]
+        #     auth_code = params[:code]
+        #     access_token = oauth.authenticate! auth_code
+        #
+        #
+        # @client = Slice::Client.new(access_token: access_token.token)
+        # @client.list_items
+        # @client.list_orders
+        # @client.create_order(orderTitle: 'Your Amazon.com order')
+        # @client.get_order(123)
+        # @client.update_order(123, orderTotal: 395)
+        # @client.list_items(limit: 3, offset: 10)
+        # @client.list_items(limit: 3, offset: 10).status
+        # @client.list_items(limit: 3, offset: 10).headers
+        # @client.list_items(limit: 3, offset: 10).body
+
+
+
+    #    for cli in @client
+    #     cli.list_items
+    #    end
+    # end
+
   end
 end
--- ultimate-money-slicer/app/helpers/welcome_helper.rb
+++ ultimate-money-slicer/app/helpers/welcome_helper.rb
@@ -1,2 +1,23 @@
 module WelcomeHelper
+    # require 'uri'
+    #
+    # def doIt
+    #   uri = URI.parse(@auth_url)
+    #   auth_code = uri.host.split('=').first
+    #
+    #   if @auth_url != nil
+    #     access_token = @oauth.authenticate! auth_code
+    #   end
+    #
+    #   @client = Slice::Client.new(access_token: access_token.token)
+    #   @client.list_items
+    #   @client.list_orders
+    #   @client.create_order(orderTitle: 'Your Amazon.com order')
+    #   @client.get_order(123)
+    #   @client.update_order(123, orderTotal: 395)
+    #   @client.list_items(limit: 3, offset: 10)
+    #   @client.list_items(limit: 3, offset: 10).status
+    #   @client.list_items(limit: 3, offset: 10).headers
+    #   @client.list_items(limit: 3, offset: 10).body
+    # end
 end
