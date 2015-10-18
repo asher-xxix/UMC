c69e97fd3e868dfb66c3b1fb3464fe7417c93586
lunete <claud@openmailbox.org>
2015-10-18 10:50:55 -0400

    oauth loop completed

--- ultimate-money-slicer/app/controllers/slice_controller.rb
+++ ultimate-money-slicer/app/controllers/slice_controller.rb
@@ -1,4 +1,27 @@
 class SliceController < ApplicationController
     def slice
+        require 'uri'
+
+        uri = URI.parse(@auth_url)
+        auth_code = uri.host.split('=').first
+
+        access_token = oauth.authenticate! auth_code
+
+
+        @client = Slice::Client.new(access_token: access_token.token)
+        @client.list_items
+        @client.list_orders
+        @client.create_order(orderTitle: 'Your Amazon.com order')
+        @client.get_order(123)
+        @client.update_order(123, orderTotal: 395)
+        @client.list_items(limit: 3, offset: 10)
+        @client.list_items(limit: 3, offset: 10).status
+        @client.list_items(limit: 3, offset: 10).headers
+        @client.list_items(limit: 3, offset: 10).body
+
+
+
+        redirect_to('http://localhost:3000')
+
     end
 end
--- ultimate-money-slicer/app/controllers/welcome_controller.rb
+++ ultimate-money-slicer/app/controllers/welcome_controller.rb
@@ -1,37 +1,39 @@
 class WelcomeController < ApplicationController
   def homepage
       require 'slice'
-      #require 'uri'
+    #   require 'uri'
 
 
 
-      oauth = Slice::OAuth.new("47d4d088", "0be08d72fd5cdb21f08eea76d2479832", "http://localhost:3000")
+      oauth = Slice::OAuth.new("47d4d088", "0be08d72fd5cdb21f08eea76d2479832", "http://localhost:3000/welcome/slice")
       @auth_url = oauth.generate_auth_url
 
 
 
-      #uri = URI.parse(@auth_url)
-      #auth_code = uri.host.split('=').first
+    #   uri = URI.parse(@auth_url)
+    #   auth_code = uri.host.split('=').first
       #if oauth.code != nil
 
 
 
         # params.require[:code]
+        # if params[:code] != ""
         #     auth_code = params[:code]
-        #     access_token = oauth.authenticate! auth_code
-        #
-        #
-        # @client = Slice::Client.new(access_token: access_token.token)
-        # @client.list_items
-        # @client.list_orders
-        # @client.create_order(orderTitle: 'Your Amazon.com order')
-        # @client.get_order(123)
-        # @client.update_order(123, orderTotal: 395)
-        # @client.list_items(limit: 3, offset: 10)
-        # @client.list_items(limit: 3, offset: 10).status
-        # @client.list_items(limit: 3, offset: 10).headers
-        # @client.list_items(limit: 3, offset: 10).body
-
+        # if auth_code != ""
+            # access_token = oauth.authenticate! auth_code
+            #
+            #
+            # @client = Slice::Client.new(access_token: access_token.token)
+            # @client.list_items
+            # @client.list_orders
+            # @client.create_order(orderTitle: 'Your Amazon.com order')
+            # @client.get_order(123)
+            # @client.update_order(123, orderTotal: 395)
+            # @client.list_items(limit: 3, offset: 10)
+            # @client.list_items(limit: 3, offset: 10).status
+            # @client.list_items(limit: 3, offset: 10).headers
+            # @client.list_items(limit: 3, offset: 10).body
+        # end
 
 
     #    for cli in @client
